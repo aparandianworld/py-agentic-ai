fe3a3c8a074798788b9b5e45f40f9ed4deef98c1
Aaron Parandian <aaron.parandian.usa@gmail.com>
2025-12-19 16:54:08 -0800

    02. define web search tool and stand alone test

--- tools.py
+++ tools.py
@@ -8,10 +8,24 @@ def web_search(query: str, num_results: int) -> str:
         with DDGS() as ddgs:
             search_results = ddgs.text(query, max_results=num_results)
             for result in search_results:
-                results.append(result)
+                results.append(
+                    {
+                        "title": result["title"],
+                        "href": result["href"],
+                        "body": result["body"],
+                    }
+                )
     except Exception as e:
         print(f"Error performing web search: {e}")
     if not results:
         return "No search results found."
 
-    return json.dumps(results)
+    formatted = f"search result for: {query}\n"
+    for i, result in enumerate(results):
+        formatted += "\n"
+        formatted += f"Result {i + 1}:\n"
+        formatted += f"Title: {result['title']}\n"
+        formatted += f"URL: {result['href']}\n"
+        formatted += f"Summary: {result['body']}\n"
+        formatted += "\n"
+    return formatted.strip()
